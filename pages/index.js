75adc118acffa735830b4f4b09587fe7111ec8b5
Furkan Beydemir <70379120+beydemirfurkan@users.noreply.github.com>
2023-01-29 15:23:18 +0300

    ff

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,9 @@ import Layout from '../components/Layout'
 
 const index = () => {
   return (
-    <div>
       <Layout>
           <h1 className="title">Index</h1>
       </Layout>
-    </div>
 
   )
 }
